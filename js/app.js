1fa35f7fc37d8ca0c6bbe0e1b7b1a8dab9a22f3d
Nilxxx11 <80600547+Nilxxx11@users.noreply.github.com>
2024-05-13 19:53:42 -0500

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -35,33 +35,37 @@ cancel.addEventListener("click", showRegisterModal);
 const handleEditStudent = (key) => {
   const studentRef = ref(getDatabase(), `/${key}`);
 
-  // Obtener datos del estudiante una vez
-  get(studentRef)
-    .then((snapshot) => {
-      // Verificar si el snapshot no está vacío
-      if (snapshot.exists()) {
+  try {
+    // Obtener datos del estudiante una vez
+    get(studentRef)
+      .then((snapshot) => {
         const student = snapshot.val();
 
-        // Precargar los campos del formulario con los datos del estudiante
-        form['placa'].value = student.placa;
-        form['rif'].value = student.rif;
-        form['gps'].value = student.gps;
-        form['camara'].value = student.camara;
-        form['fecha'].value = student.fecha;
-        form['detalles'].value = student.detalles;
+        // Verificar si se obtuvieron los datos correctamente
+        if (student) {
+          // Precargar los campos del formulario con los datos del estudiante
+          form['placa'].value = student.placa;
+          form['rif'].value = student.rif;
+          form['gps'].value = student.gps;
+          form['camara'].value = student.camara;
+          form['fecha'].value = student.fecha;
+          form['detalles'].value = student.detalles;
 
-        // Asignar la clave del registro que se está editando
-        editKey = key;
+          // Asignar la clave del registro que se está editando
+          editKey = key;
 
-        // Mostrar el modal
-        modal.classList.add('is-active');
-      } else {
-        console.error('No se encontraron datos para el estudiante con la clave:', key);
-      }
-    })
-    .catch((error) => {
-      console.error('Error al obtener datos del estudiante: ', error);
-    });
+          // Mostrar el modal
+          modal.classList.add('is-active');
+        } else {
+          console.error('No se encontraron datos para el estudiante con la clave:', key);
+        }
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos del estudiante: ', error);
+      });
+  } catch (error) {
+    console.error('Error al obtener datos del estudiante:', error);
+  }
 };
 
 // Función para manejar la eliminación de estudiantes
